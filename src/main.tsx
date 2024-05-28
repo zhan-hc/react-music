888d6f60eead10ce2a8a0c613df1bc17c923dd86
詹焕超 <huanchao_zhan@feidee.com>
2024-05-28 17:44:49 +0800

    fix: 增加环境配置

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,17 @@
-import { BrowserRouter} from 'react-router-dom';
+import { BrowserRouter} from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import 'antd/dist/reset.css'
 import './assets/style/theme.scss'
-import { Provider } from 'react-redux';
+import { Provider } from 'react-redux'
 import store from './store'
-import { LoadingProvider } from './components/loading/LoadingContext';
+import { LoadingProvider } from './components/loading/LoadingContext'
+
+const basename = import.meta.env.VITE_BASE_URL
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
       <LoadingProvider>
-        <BrowserRouter> 
+        <BrowserRouter basename={basename}> 
           <App />
         </BrowserRouter>
       </LoadingProvider>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import { resolve } from "path"
 import react from '@vitejs/plugin-react'
-
+const env = process.env.NODE_ENV
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -10,4 +10,5 @@ export default defineConfig({
       '@': resolve(__dirname, './src')
     },
   },
+  base: env === 'production' ? '/project/react-music/' : '', // 配置是因为部署需要在对应的路径下访问
 })
